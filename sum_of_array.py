10893ca0582e620cdc2f4a1a859dd20dafb731a9
Mohit4458 <133806656+Mohit4458@users.noreply.github.com>
2023-06-02 00:09:23 +0530

    Create sum_of_array.py

---
+++ sum_of_array.py
@@ -0,0 +1,10 @@
+# input values to list
+arr = [2, 30, 18, 5]
+
+# sum() is an inbuilt function in python that adds
+# all the elements in list,set and tuples and returns
+# the value
+ans = sum(arr)
+
+# display sum
+print('Sum of the array is ', ans)
