4517de0f69b8d3fb1db6a58b7657a4ae9f9f990c
Mohit4458 <133806656+Mohit4458@users.noreply.github.com>
2023-06-01 23:15:14 +0530

    Create Smallestnum.py

---
+++ Smallestnum.py
@@ -0,0 +1,17 @@
+# Python program to find smallest
+
+# number in a list
+
+ 
+
+# list of numbers
+
+list1 = [11, 2,10, 5, 109]
+
+ 
+
+ 
+
+# printing the smallest element
+
+print("Smallest element is:", min(list1))
