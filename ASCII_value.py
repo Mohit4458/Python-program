e149e8340d223c84ce99d8464f2481f5ffcdbe1f
Mohit4458 <133806656+Mohit4458@users.noreply.github.com>
2023-06-01 23:28:30 +0530

    Create ASCII_value.py

---
+++ ASCII_value.py
@@ -0,0 +1,9 @@
+# Python program to print 
+# ASCII Value of Character
+
+# In h we can assign different
+# characters of which we want ASCII value 
+
+h = 'p'
+# print the ASCII value of assigned character in h
+print("The ASCII value of '" + c + "' is", ord(h))
